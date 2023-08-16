b781422e15bb95709989b81c4290e8d30e84b51d
Laura C. Balbachan dos Santos <balbachanlaura@gmail.com>
2023-08-16 16:04:14 -0300

    Initial Commit

---
+++ supportclone_AL/supportclone_ALApp.swift
@@ -0,0 +1,17 @@
+//
+//  supportclone_ALApp.swift
+//  supportclone_AL
+//
+//  Created by Laura C. Balbachan dos Santos on 16/08/23.
+//
+
+import SwiftUI
+
+@main
+struct supportclone_ALApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
